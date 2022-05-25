fe5334a391537f89978ede4dd0aae2abe2e56e81
Itai Simhony <itiaz05@gmail.com>
2022-05-25 15:48:44 +0300

    api.js added, moving to Flask

--- code/client/chatbot-app/src/App.js
+++ code/client/chatbot-app/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Fab from "@mui/material/Fab";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello! I'm Your Amdocs Bot</h1>
+      <TextareaAutosize
+        aria-label="messages textarea"
+        placeholder="Hi!"
+        style={{ width: 200 }}
+      />
+
+      <Fab
+        className="chatIcon"
+        size="small"
+        color="secondary"
+        aria-label="chatbot"
+      >
+        <PersonOutlineIcon />
+      </Fab>
     </div>
   );
 }
---
+++ code/client/chatbot-app/src/api.js
@@ -0,0 +1,35 @@
+import Axios from 'axios';
+
+const $axios = Axios.create({
+    baseURL: '/api/',
+    headers: { 'Content-Type': 'application/json' },
+});
+
+
+const getOnBeforeRequestHandler = () => config => config;
+
+const onRequestErrorHandler = () => error => Promise.reject(error);
+
+const getOnResponseHandler = () => response => response;
+
+const onResponseErrorHandler = () => error => {
+    throw error;
+};
+
+$axios.interceptors.request.use(getOnBeforeRequestHandler(), onRequestErrorHandler());
+$axios.interceptors.response.use(getOnResponseHandler(), onResponseErrorHandler());
+
+class ChatBox {
+  constructor() {
+    this.args = {
+      openButton: document.querySelector(".chatbox_button"),
+      chatBox: document.querySelector(".chatbox_support"),
+      sendButton: document.querySelector(".send_button"),
+    };
+    this.state = false;
+    this.messages = [];
+  }
+}
+
+const apiService = { ChatBox };
+export default apiService;
